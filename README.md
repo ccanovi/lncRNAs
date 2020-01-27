41df4ed8e05c8890fa8f0a38a71d32fe594d98d6
Nicolas <nicolas.delhomme@umu.se>
2020-01-27 14:43:04 +0100

    Added a script to index the swissprot database needed for the PLncPRO tool

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # lncRNAs
 stuff useful for the pipeline
+
+## Setup
+```{bash}
+ln -s /mnt/picea/storage/reference/Swiss-Prot swissprot
+```
+
+
+## Databases
+### Swiss-prot
+Retrieved from [UniProt](ftp.uniprot.org)
---
+++ pipeline/submitBlastIndex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+## error and verbose
+set -ex
+
+# modules
+module load bioinfo-tools blast
+
+## default args
+mail="camilla.canovi@umu.se"
+in=../swissprot/2019_11/fasta/uniprot_sprot.fasta.gz
+out=../swissprot/2019_11/indices/blast+
+
+## create the out dir
+if [ ! -d $out ]; then
+    mkdir -p $out
+fi
+
+## prepare
+sbatch --mail-user $mail -e $out/index.err -o $out/index.out \
+../UPSCb-common/pipeline/runBlastPlusMakeblastdb.sh -p prot -t swissprot $in $out
+
