4273f64ee902a53c46fe6b61927d5150bbd28bb5
Nicolas <nicolas.delhomme@umu.se>
2021-03-17 18:27:42 +0100

    Did some extra analysis

--- src/R/lincRNAPlantGenIEBlastResults.R
+++ src/R/lincRNAPlantGenIEBlastResults.R
@@ -29,11 +29,15 @@ length(unique(res$df$query.id))
 #' to 57286 subjects :-O
 length(unique(res$df$subject.id))
 
-#' Only 37 have reciprocal coverage over 70%
-sel <- res$df$subject.cum.cov >= 0.7 & res$df$query.cum.cov >= 0.7
+#' Only 3647 have a query coverage over 70%
+sel <- res$df$query.cum.cov >= 0.7
+sum(sel)
 
 #' The ones without species are Pinus taeda, and there are a few from Ginkgo
-unique(res$df[sel,"subject.id"])
+res$df$Species <- "Pinus_taeda"
+sel2 <- grepl("\\|",res$df$subject.id)
+res$df$Species[sel2] <- sub(".*\\|","",res$df$subject.id[sel2])
+res$df[res$df$Species=="","Species"] <- "Pinus_taeda"
 
 #' How many species and what's their average coverage
 res$df$Species <- "Pinus_taeda"
@@ -43,11 +47,25 @@ res$df[res$df$Species=="","Species"] <- "Pinus_taeda"
 
 mar=par("mar")
 par(mar=c(10.1,4.1,0.1,0.1))
-boxplot(split(res$df$query.cum.cov,res$df$Species),las=2,ylim=c(0,1))
 
+#' all
+boxplot(split(res$df$query.cum.cov,res$df$Species),las=2,ylim=c(0,1))
 boxplot(split(res$df$query.cum.cov,res$df$Species),notch=TRUE,las=2,ylim=c(0,0.4))
 
+#' over 70% coverage
+boxplot(split(res$df$query.cum.cov[sel],res$df$Species[sel]),las=2,ylim=c(0,1))
+
+#' 707 unique lncRNAs
+length(unique(res$df$query.id[sel]))
+
+#' number of hits per lncRNAs (number on the x axis, the y axis is the number of lincRNAs with that many hits)
+sp <- split(res$df$Species[sel],res$df$query.id[sel])
 par("mar"=mar)
+barplot(table(elementNROWS(sp)))
+
+# number of species per lncRNA
+barplot(table(elementNROWS(lapply(sp,unique))))
+barplot(table(elementNROWS(lapply(sp,unique))),log="y")
 
 #' # Session Info
 #' ```{r session info, echo=FALSE}
